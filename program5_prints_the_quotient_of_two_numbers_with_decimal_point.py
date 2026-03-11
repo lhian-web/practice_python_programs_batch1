29a6eb6026a2a565da20299f418390da646d1e24
unknown <jhullianchica@gmail.com>
2026-03-11 13:47:51 +0800

    Added input, divide numbers, and print quotient

--- program5_prints_the_quotient_of_two_numbers_with_decimal_point.py
+++ program5_prints_the_quotient_of_two_numbers_with_decimal_point.py
@@ -1,3 +1,9 @@
 # Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 # Divide num1 to num2
+quotient = num1 / num2
+
 # Print the quotient
+print("The quotient is:", quotient)
