1359bf01bdfafbbabef76917c1703620b5fe2030
unknown <jhullianchica@gmail.com>
2026-03-11 13:02:10 +0800

    Added input and checks if numbers are equal

--- program2_checks_if_two_numbers_are_equal.py
+++ program2_checks_if_two_numbers_are_equal.py
@@ -1,2 +1,9 @@
 # Input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # Check if the numbers are equal
+if num1 == num2:
+    print("The numbers are equal")
+else:
+    print("The numbers are not equal")
