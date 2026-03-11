918dcd13eee341f496073e155450f63c7f08d0f5
unknown <jhullianchica@gmail.com>
2026-03-11 14:43:53 +0800

    Added odd count variable, input numbers, check for odd numbers, print count

--- program8_prints_the_count_of_odd_numbers.py
+++ program8_prints_the_count_of_odd_numbers.py
@@ -1,4 +1,12 @@
 # Odd count variable
+odd_count = 0
+
 # Input numbers
-# Check if number is odd
+for i in range(10):
+    num = int(input("Enter number: "))
+
+    if num % 2 != 0: # Check if number is odd
+        odd_count += 1
+
 # Print odd count
+print("Number of odd numbers:", odd_count)
