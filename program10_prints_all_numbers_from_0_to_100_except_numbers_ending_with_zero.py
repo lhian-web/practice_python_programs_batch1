7df6845845f20b4c0dce9c5ebf1dd1660ce68a75
unknown <jhullianchica@gmail.com>
2026-03-11 14:59:56 +0800

    Added 0 to 100 loop, check if number ends with 0, and print numbers not ending with 0

--- program10_prints_all_numbers_from_0_to_100_except_numbers_ending_with_zero.py
+++ program10_prints_all_numbers_from_0_to_100_except_numbers_ending_with_zero.py
@@ -1,3 +1,6 @@
 # For loop from 0 to 100
-# Check if number ends with 0
-# Print numbers not ending with 0
+for i in range(101):
+    # Check if number ends with 0
+    if i % 10 != 0:
+        # Print numbers not ending with 0
+        print(i)
