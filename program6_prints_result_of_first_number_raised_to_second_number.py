2cc48c7fbd4d3deae23920ab2efc2148ebd7bf99
unknown <jhullianchica@gmail.com>
2026-03-11 13:58:16 +0800

    Added input, exponent operation, print result

--- program6_prints_result_of_first_number_raised_to_second_number.py
+++ program6_prints_result_of_first_number_raised_to_second_number.py
@@ -1,3 +1,9 @@
 # Input two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
 # Raise num1 to num2
+result = num1 ** num2
+
 # print result
+print("The result is:", result)
