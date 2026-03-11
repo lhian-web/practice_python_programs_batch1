560dcbc35af31f931fbd821753b1c6a998a09956
unknown <jhullianchica@gmail.com>
2026-03-11 13:41:38 +0800

    Added input, multiply two numbers, and print product

--- program4_prints_the_product_of_two_numbers.py
+++ program4_prints_the_product_of_two_numbers.py
@@ -1,3 +1,9 @@
 # Input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # Multiply two numbers
+product = num1 * num2
+
 # Print the product of two numbers
+print("The product is:", product)
