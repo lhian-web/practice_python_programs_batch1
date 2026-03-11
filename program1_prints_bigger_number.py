5833aeb61738757e54ba57cdc46c8a01ef3e0b42
unknown <jhullianchica@gmail.com>
2026-03-11 12:57:41 +0800

    Added input and compare numbers

--- program1_prints_bigger_number.py
+++ program1_prints_bigger_number.py
@@ -1,2 +1,9 @@
 # Input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # Compare two numbers and print the bigger number
+if num1 > num2:
+    print("The bigger number is:", num1)
+else:
+    print("The bigger number is:", num2)
